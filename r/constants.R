46546e956d0e691664e6126f02245d3d83f0aadb
Chat <chat@ifi.uzh.ch>
2019-01-21 07:44:54 +0100

    Added plot combination and label arrangements

--- r/constants.R
+++ r/constants.R
@@ -4,10 +4,22 @@ library(cleanslate) # devtools::install_github("chatchavan/cleanslate@v0.1.0")
 types_data_qual <- c("qualraw", "qualcoded", "qualcodebook")
 types_data_quan <- c("quanraw", "quanprocessed", "quancode")
 types_all <- c("study",  types_data_qual, types_data_quan, "software", "hardware")
+labels_all <- c(
+  "Study materials",
+  "Raw qualiative data",
+  "Coded qualitative data",
+  "Coding manual for qualitative data",
+  "Raw quantitative data",
+  "Processed quantitative data",
+  "Data analysis source code",
+  "Software prototypes",
+  "Hardaware prototypes"
+)
 
 persist(types_data_qual)
 persist(types_data_quan)
 persist(types_all)
+persist(labels_all)
 
 
 # overall analysis settings
@@ -27,7 +39,12 @@ message("Analyzing all data, including those with mismatched expertise level. (c
 theme_set(theme_grey(base_size = 7))
 
 # ensure that all material types are shown
-scale_x_material_type <- scale_x_discrete(drop = FALSE, limits = rev(types_all))
+scale_x_material_type <-
+  scale_x_discrete(
+    drop = FALSE,
+    breaks = rev(types_all),
+    labels = rev(labels_all))
+
 persist(scale_x_material_type)
 
 # color palette: https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
@@ -74,3 +91,7 @@ scale_color_20 <-  scale_color_manual(values = c(
 "#ffd8b1",
 "#000080"))
 persist(scale_color_20)
+
+
+scale_fill_availability <- scale_fill_manual(values = c("#D55E00", "#009E73"), breaks = c("public", "private"))
+persist(scale_fill_availability)
--- r/sharing_prob_by_type.R
+++ r/sharing_prob_by_type.R
@@ -3,7 +3,7 @@ library(cleanslate) # devtools::install_github("chatchavan/cleanslate@v0.1.0")
 import::from(glue, glue)
 import::from(broom, tidy)
 import::from(PropCIs, exactci)
-
+import::from(cowplot, plot_grid, get_legend)
 
 source("r/constants.R")
 source("r/io.R")
@@ -12,7 +12,8 @@ source("r/io.R")
 #===============================================================================
 # retrieve availability frequency
 df_all <-
-  load_data(limesurvey_export_path, exclude_mismatch = FALSE)
+  load_data(limesurvey_export_path, exclude_mismatch = FALSE) %>%
+  mutate(type = factor(type, levels = rev(types_all)))
 
 # (For reference: 665 inivitations; 222 responses; of which 29 didn’t generate any research material)
 
@@ -35,8 +36,6 @@ avail_prob <-
   select(type, public = p_public, private = p_private) %>%
   gather("availability", "probability", public, private)
 
-View(avail_prob)
-
 avail_prob <-
   avail_prob %>%
   group_by(type) %>%
@@ -44,11 +43,60 @@ avail_prob <-
     label = sprintf("%2.0f %%", probability * 100),
     label_pos = cumsum(probability) - 0.5 * probability)
 
-p_tmp <-
+p_avail_prob_by_type <-
   avail_prob %>%
-  ggplot(aes(x = type, y = probability, fill = availability)) +
+  ggplot(aes(x = fct_inorder(type), y = probability, fill = availability)) +
   geom_bar(stat = "identity") +
-  geom_text(aes(label = label, y = label_pos), size = 2) +
+  geom_text(aes(label = label, y = label_pos), size = 2, color = "white") +
+  scale_x_material_type +
+  scale_fill_availability +
+  coord_flip() +
+  xlab(NULL) +
+  theme(legend.position = "bottom")
+
+ggsave("output/avail_prob_by_type.pdf", p_avail_prob_by_type, height = 200/72, width = 300/72, unit = "in", dpi = 72)
+
+
+#===============================================================================
+# CI for the proportion
+
+
+public_by_type_ci <-
+  avail_prob_count %>%
+  rowwise() %>%
+  do({
+    row_type <- .$type[[1]]
+    exactci(.$public[[1]], .$paper_count[[1]], 0.95) %>%
+      tidy() %>%
+      mutate(type = row_type)
+  })
+
+
+p_availability_by_type_proportion <-
+  public_by_type_ci %>%
+  ggplot(aes(x = fct_inorder(type), ymin = conf.low, ymax = conf.high)) +
+  geom_errorbar(width = 0.5) +
+  geom_text(data = avail_prob_count, inherit.aes = FALSE,
+    aes(x = fct_inorder(type), y = 1, label = paste("n =", paper_count)), hjust = "right", size = 3) +
+  scale_x_material_type +
+  ylim(0, 1) +
+  xlab(NULL) +
+  ylab("Clopper-Pearson exact 95% CI of the proportion of public materials") +
   coord_flip()
 
-ggsave("output/avail_prob_by_type.pdf", p_tmp, height = 200/72, width = 300/72, unit = "in", dpi = 72)
+ggsave("output/availability_by_type_proportion.pdf", p_availability_by_type_proportion, height = 150/72, width = 300/72, unit = "in", dpi = 72)
+
+
+#===============================================================================
+# combine the plots
+
+legend <- get_legend(p_avail_prob_by_type)
+p_two_cols <-
+  plot_grid(
+    p_avail_prob_by_type + theme(legend.position = "none"),
+    p_availability_by_type_proportion + theme(legend.position = "none", axis.text.y = element_blank(), axis.ticks.y = element_blank()),
+    # p_response_count,
+    nrow = 1, rel_widths = c(0.5, 0.45, 0.05))
+
+p_availability <- plot_grid(p_two_cols, legend, nrow = 2, rel_heights = c(3, 0.3))
+ggsave("output/availability.pdf", p_availability, height = 150/72, width = 600/72, unit = "in", dpi = 72)
